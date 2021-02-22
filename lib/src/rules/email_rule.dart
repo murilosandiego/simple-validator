b615030ec5dede8239dd2213b2c972b6e425e9b4
Murilo Sandiego <murilosandiego1@gmail.com>
2021-02-22 12:09:12 -0300

    Fix Email Rule

--- lib/src/rules/email_rule.dart
+++ lib/src/rules/email_rule.dart
@@ -9,7 +9,7 @@ class EmailRule extends BaseRule {
   bool validate(String text) {
     return RegexRule(
             pattern:
-                "^[a-zA-Z0-9.!#\$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*\$")
+                r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$')
         .validate(text);
   }
 }
--- test/src/rules/email_rule_test.dart
+++ test/src/rules/email_rule_test.dart
@@ -8,14 +8,13 @@ void main() {
       "firstname.lastname@example.com",
       "email@subdomain.example.com",
       "firstname+lastname@example.com",
-      "email@123.123.123.123",
       "1234567890@example.com",
       "email@example-one.com",
       "_______@example.com",
       "email@example.name",
       "email@example.museum",
       "email@example.co.jp",
-      "firstname-lastname@example.com"
+      "firstname-lastname@example.com",
     ];
 
     for (var email in emails) {
@@ -26,12 +25,14 @@ void main() {
     List<String> wrongEmails = [
       "plainaddress",
       "#@%^%#\$@#\$@#.com",
+      "email@123.123.123.123",
       "@example.com",
       "Joe Smith <email@example.com>",
       "email.example.com",
-      "あいうえお@example.com",
       "email@example.com (Joe Smith)",
-      "email@example..com"
+      "email@example..com",
+      "email@example.",
+      "email@example",
     ];
     for (var email in wrongEmails) {
       final error = EmailRule().validate(email);
